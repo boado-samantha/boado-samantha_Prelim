1e1202f3e86fa190f83e62c91faeff1c1ac49199
boado-samantha <s2022102430@firstasia.edu.ph>
2024-08-29 11:30:53 -0400

    Calculator

---
+++ Activity 2/index.js
@@ -0,0 +1,31 @@
+function calculate() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const operator = document.getElementById('operator').value;
+    let result;
+
+    switch (operator) {
+        case 'add':
+            result = num1 + num2;
+            break;
+        case 'subtract':
+            result = num1 - num2;
+            break;
+        case 'multiply':
+            result = num1 * num2;
+            break;
+        case 'divide':
+            if (num2 !==0) {
+                result = num1 / num2;
+            } else {
+                result = 'Cannot divide by zero';
+            }
+            break;
+        default:
+            result = 'Invalid operator';
+            break;
+    }
+
+
+    document.getElementById('result').innerText = result;
+}
